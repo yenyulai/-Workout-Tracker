71cfd8fe90764975bc64ccff6afeec25e3213b84
Laiyenyu <yenyulai98@gmail.com>
2021-02-19 21:00:13 -0600

    update mongoose connection

--- server.js
+++ server.js
@@ -14,7 +14,12 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", 
+{ 
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false });
 
 // routes
 app.use(require("./route/api.js"));
